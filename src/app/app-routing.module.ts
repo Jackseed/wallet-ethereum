15805d3b12d301dc0f1062e49c8c1d8d7e0cc8ea
Jackseed <vinkho.c@gmail.com>
2018-11-03 14:08:31 +0100

    angular project all set up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MnemonicComponent } from './wallet/containers/mnemonic/mnemonic.component';
+import { VerificationComponent } from './wallet/containers/verification/verification.component';
+import { WalletComponent } from './wallet/containers/wallet/wallet.component';
+import { TransactionComponent } from './wallet/containers/transaction/transaction.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/wallet', pathMatch: 'full' },
+  { path: 'mnemonic', component: MnemonicComponent },
+  { path: 'verification', component: VerificationComponent },
+  { path: 'wallet', component: WalletComponent },
+  { path: 'transaction', component: TransactionComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MnemonicComponent } from './wallet/containers/mnemonic/mnemonic.component';
+import { VerificationComponent } from './wallet/containers/verification/verification.component';
+import { WalletComponent } from './wallet/containers/wallet/wallet.component';
+import { TransactionComponent } from './wallet/containers/transaction/transaction.component';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MnemonicComponent,
+    VerificationComponent,
+    WalletComponent,
+    TransactionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
