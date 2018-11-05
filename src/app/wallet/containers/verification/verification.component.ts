fcfe6380f1420c99d4b68a378bd62bdf01071697
Jackseed <vinkho.c@gmail.com>
2018-11-05 16:57:00 +0100

    verification page
    not responsive yet

--- src/app/wallet/containers/verification/verification.component.ts
+++ src/app/wallet/containers/verification/verification.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Mnemonic {
+  id: number;
+  name: string;
+  input_id: number;
+}
 
 @Component({
   selector: 'app-verification',
@@ -7,6 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verification.component.css']
 })
 export class VerificationComponent implements OnInit {
+
+  mnemonics: Mnemonic[] = [
+    {id: 4, name: 'Mnemonic n°4', input_id:1},
+    {id: 9, name: 'Mnemonic n°9', input_id:2},
+    {id: 12, name: 'Mnemonic n°12', input_id:3}
+  ];
+
 
   constructor() { }
 
